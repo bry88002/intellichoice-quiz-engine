98a6ba0c93adbfa00af025b19e55957da75fcf80
RRRRRRRRJIN <54645360+RRRRRRRRJIN@users.noreply.github.com>
2019-11-20 11:35:54 -0600

    addProfile

---
+++ newclient/src/App.js
@@ -0,0 +1,118 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Route render={({ location }) => {
+                const path = location.pathname;
+                if (path === '/dashboard' || path === '/profile') return;
+                return (
+                    <div>
+                        <h1>Path not found</h1>
+                        <Link to="/dashboard">Click here to go to the dashboard</Link>
+                        <div style={{ borderStyle: "solid", height: "2em", width: "150px", textAlign: "center" }} onClick={() => { alert("hi"); }}>hello</div>
+                    </div>
+                )
+            }} />
+            <Route exact={true} path="/dashboard" component={dashboard} />
+            <Route exact={true} path="/profile" component={profile} />
+        </BrowserRouter>
+    );
+}
+
+const dashboard = (props) => (
+    <div>
+        <div className="header">
+            <img id="intellichoice-logo" src="https://www.intellichoice.org/wp-content/uploads/2019/01/intellichoice-logo-white-1024x1024.png" alt="intellichoice logo" />
+            <Link to='/profile'>
+                <div style={{ borderStyle: 'none', float: 'right', height: '3em', marginRight: '0.5em', marginTop: '0.5em' }}>
+                    <h4 style={{ color: 'white' }}>Profile</h4>
+                </div>
+            </Link>
+        </div>
+
+        <div>
+            <div className="small-header-left">
+                <h1 style={{ color: 'white', paddingTop: '0.25em' }}>My Achievements</h1>
+            </div>
+
+            <div className="small-header">
+                <h1 style={{ color: 'white', paddingTop: '0.25em' }}>My Quiz List</h1>
+            </div>
+
+            <div className="achievement-box">
+                <h1>My Rank: 1st</h1>
+                <h1>My Score: 100%</h1>
+                <h1>Correctness:</h1>
+                <img id="correctness-pie-chart" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/English_dialects1997.svg/1200px-English_dialects1997.svg.png" alt='pie chart of countries' />
+            </div>
+
+            <QuizBox title='Fractions' questionNumber='15' status='100' />
+            <QuizBox title='Addition' questionNumber='10' status='100' />
+            <QuizBox title='Subtraction' questionNumber='20' status='50' />
+            <QuizBox title='Multiplication' questionNumber='15' status='50' />
+        </div>
+    </div>
+);
+
+const QuizBox = (props) => {
+    return (
+        <div className='quiz-box' style={{ cursor: 'pointer' }} onClick={() => { alert('You are now taking the quiz'); }} className="quiz-box">
+            <img src="https://i.groupme.com/583x385.png.28c4abc1f9ad4b09a1f593c581c292e8.large" alt="books" />
+            <h3 style={{ paddingTop: '0.3em' }}>{props.title}</h3>
+            <hr className='col-xs-12' style={{ width: '60%', borderWidth: '5px', borderColor: 'black' }} />
+            <p style={{ float: 'left' }}>{props.questionNumber} questions</p>
+            <p style={{ float: 'right', marginRight: '2%' }}>Status: {props.status}%</p>
+        </div>
+    );
+};
+
+const profile = () => {
+    return (
+        <div className='profile-box'>
+            <div className='profile-page-top'>
+
+
+            </div>
+
+            <div className='profile-page-middle'>
+                <img className="profile-picture" src="https://i.groupme.com/283x283.png.95232a45f8a744e49556247eca804d28.large" alt="profile"></img>
+                <p className="username">Bryanth Fung</p>
+            </div>
+
+            <div className='profile-page-bottom'>
+                <div className="info">
+                    <div className="info-title">
+                        <p className="infomation">info</p>
+                    </div>
+                    <div className="info-info">
+                        <p>Email:hhh@utdallas.edu<br />
+                            Grade:5</p>
+                    </div>
+                    <p className="changeLink">Change</p>
+                </div>
+
+                <div className="learning">
+                    <div className="learning-title">
+                        <p className="learning-status">Learning Status</p>
+                    </div>
+
+                    <div className="info-box">
+                        <p className="learning-info">Rank:<br />Total score:<br />Correctness:<br />Current quiz:</p>
+                    </div>
+                </div>
+
+            </div>
+            <p>Click <Link to="/dashboard">here</Link> to go back to the dashboard</p>
+        </div>
+
+
+    )
+};
+
+export default App;
